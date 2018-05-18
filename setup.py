46e4b0eb9a095e49449e500150a2430675ffd2c1
surfer190 <shutch190@gmail.com>
2018-05-18 15:14:18 +0200

    Add gitignore and update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-  name = 'eodclient',
-  packages = ['eodclient'],
-  version = '1.0.2.dev1',
-  description = 'A python client for interacting with the EOD Historical Data api',
-  author = 'surfer190',
-  author_email = 'shutch190@gmail.com',
-  url = 'https://github.com/surfer190/EOD-Historical-Data-client',
-  download_url = 'https://github.com/surfer190/EOD-Historical-Data-client/archive/1.0.2.dev1.tar.gz',
-  keywords = ['api', 'client', 'eod', 'eod historical data'],
-  classifiers = [],
-  license='GPLv3',
-  install_requires=['requests>=2,<3'],
-  python_requires='>=3.6',
+    name = 'eodclient',
+    packages = ['eodclient'],
+    version = '1.0.2.dev1',
+    description = 'A python client for interacting with the EOD Historical Data api',
+    author = 'surfer190',
+    author_email = 'shutch190@gmail.com',
+    url = 'https://github.com/surfer190/EOD-Historical-Data-client',
+    download_url = 'https://github.com/surfer190/EOD-Historical-Data-client/archive/1.0.2.dev1.tar.gz',
+    keywords = ['api', 'client', 'eod', 'eod historical data'],
+    classifiers = [],
+    license='GPLv3',
+    install_requires=['requests>=2,<3'],
+    python_requires='>=3.6',
 )
